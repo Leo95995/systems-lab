a054a4b318a4b707132cd9d227f26cbce5355e44
Leo95995 <malvolti68@gmail.com>
2025-12-08 21:37:20 +0100

    tailscale range

--- 01-vps-administration/01-setup-hardening/firewall-security.sh
+++ 01-vps-administration/01-setup-hardening/firewall-security.sh
@@ -13,7 +13,7 @@ echo "allow outgoing"
 sudo ufw default allow outgoing
 # Limite ssh . permettere sulla 22 con rate limit sulla 22
 # Proteggo da brute force
-sudo ufw limit 22/tcp
+sudo ufw allow in from 100.64.0.0/10 to any port 22 proto tcp
 # Permetto le porte http:80 e https: 443
 sudo ufw allow 80/tcp
 sudo ufw allow 443/tcp  
