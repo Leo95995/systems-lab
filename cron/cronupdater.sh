3bbb0bb6c8af0f908e9bd0aae57fb0e22ea63d29
Leo95995 <malvolti68@gmail.com>
2025-12-09 22:22:53 +0100

    feat: add simple cron to update target service

--- cron/cronupdater.sh
+++ cron/cronupdater.sh
@@ -2,6 +2,6 @@
 
 LOG_DATE=$(date +%Y%m%d_%H%M)
 
-sudo apt install && sudo apt upgrade
-
+sudo apt install -y && sudo apt upgrade -y
+ 
 echo "Aggiornamento effettuato  con successo in $LOG_DATE" >> ~/Desktop/crons/logs/cronupdater.log
