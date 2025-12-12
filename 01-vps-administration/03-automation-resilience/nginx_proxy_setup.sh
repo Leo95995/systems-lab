20e63e13ec9df51022c6f3f87d3b6b46687a5290
Leo95995 <malvolti68@gmail.com>
2025-12-12 13:41:01 +0100

    updated nginx proxy setup

--- 01-vps-administration/03-automation-resilience/nginx_proxy_setup.sh
+++ 01-vps-administration/03-automation-resilience/nginx_proxy_setup.sh
@@ -1,25 +1,43 @@
 #!/bin/bash
 
-echo "Fornire l'indirizzo pubblico di riferimento"
+# Garantisce che esca dalla procedura in caso di errore
 set -e
+
+# Path dei siti available
+NGINX_CONF_PATH="/etc/nginx/sites-available"
+# Path dei siti attivi
+NGINX_ENABLE_PATH="/etc/nginx/sites-enabled"
+
+echo "Fornire l'indirizzo pubblico di riferimento"
 # Param 1
-read PUBLIC_IP 
+read DOMAIN_NAME 
 
 echo "Inserire la porta dell'applicazione docker"
 # Param 2
-read DOCKER_PORT
+read APP_PORT
+
+# Prepara il file per gli available
+CONF_FILE="$NGINX_CONF_PATH/$DOMAIN_NAME.conf" # Questo viene dalla Fase 1
+# Prepara il file per gli enabled
+CONF_FILE_ENABLED="$NGINX_ENABLE_PATH/$DOMAIN_NAME.conf"
+
+# -L verifica se l'elemento specificato è un link simbolico.. se lo è va a rimuoverlo
+if [ -L "$CONF_FILE_ENABLED" ]; then
+    sudo rm "$CONF_FILE_ENABLED"
+    echo "Pulizia: Vecchio link simbolico rimosso."
+fi
 
 #  -z -> True se stringa vuota
 # -n -> True se stringa popolata
-if [[ -z $PUBLIC_IP || -z $DOCKER_PORT ]]; then 
+if [[ -z $DOMAIN_NAME || -z $APP_PORT ]]; then 
 
     echo "Missing non optional parameters"
     exit 1
 fi
 
 
-echo -e "\n Docker Port inserita: $DOCKER_PORT"
-echo -e "\n Ip Pubblico inserito: $PUBLIC_IP"
+echo -e "\n Docker Port inserita: $APP_PORT"
+echo -e "\n Ip Pubblico inserito: $DOMAIN_NAME"
 
 echo -e "\n Procedo a inserire creare la configurazione Nginx"
 
@@ -43,3 +61,20 @@ server {
 EOF"
 
 
+echo "File di config $DOMAIN_NAME.conf creato con successo"
+
+# Attiva la Configurazione (Symlink).
+# Verifica la Configurazione (Test).
+# Rendi Effettiva la Modifica (Reload).
+
+# Questo crea il link simbolico per collegare il file in sites/available
+# File Originale e file di destinazione
+sudo ln -s "$CONF_FILE" "$CONF_FILE_ENABLED"
+
+# Testo la sintassi di quanto inserito
+sudo nginx -t
+
+# ricarico il servizio nginx
+sudo systemctl reload nginx
+# Messaggio di conferma
+echo "Configurazione completata per $DOMAIN_NAME. Il Reverse Proxy è attivo."
