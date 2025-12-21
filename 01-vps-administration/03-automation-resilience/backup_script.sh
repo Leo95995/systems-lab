78b37fd834b75fca72131936e87e4850f693804e
Leo95995 <malvolti68@gmail.com>
2025-12-21 21:25:14 +0100

    updated backup script

--- 01-vps-administration/03-automation-resilience/backup_script.sh
+++ 01-vps-administration/03-automation-resilience/backup_script.sh
@@ -1,1 +1,12 @@
 # Script che serve per effettuare il backup
+
+# docker cp è il comando
+# test-db è il target
+# /data è il path a cui puntiamo dentro il nostro container per backup
+# ./backup e dove la roba viene messa poi sulla nostra macchina
+
+
+# docker cp test-db:/data ./backup
+
+#  I Dati utili per i container 
+# Stanno nella sezione Mounts del container quando lo inspecti
