f953ed19e7d7740b6f9ae37e504f8e65fee36393
Leo95995 <malvolti68@gmail.com>
2025-12-21 18:34:42 +0100

    script: added app service setup

--- 01-vps-administration/03-automation-resilience/app_service_setup.sh
+++ 01-vps-administration/03-automation-resilience/app_service_setup.sh
@@ -1,9 +1,76 @@
-# 2. 🛡️ Script di Resilienza: app_service_setup.sh
+##
+# Cosa fa? 
+# Crea dei servizi systemd per i container docker gia presenti e permette
+# che vengano riavviati se ex c'è un reboot della macchina
+#
+# PREREQUISITI
+# - Docker deve già essere installato sul target prescelto
+# - I container devono essere gia esistenti
+##
 
-#     Obiettivo: Creare l'unità Systemd per il tuo container Docker.
+#!/bin/bash
 
-#     Competenza Chiave: Service Management e Resilienza (SRE).
+set -e 
 
-#     Risultato Atteso: Il tuo container riparte automaticamente dopo un riavvio del server (reboot).
 
-# Se vuoi, domani sera o quando inizi, possiamo rivedere lo script nginx_proxy_setup.sh che avevamo abbozzato per assicurarci che sia perfettamente funzionante e robusto!
+# DOCKER CHECK
+if command -v docker &> /dev/null; then
+    echo -e "\n Docker è già installato. Salto l'installazione.  "
+else
+    echo "Docker e servizi non presenti"
+    exit 1
+fi
+
+
+# For each service passed is created a systemd file
+
+if [ "$#" -lt 1 ]; then
+  echo " passa almeno un parametro: Usage: $0 <container1> [container2 ...]"
+  exit 1
+fi
+
+CONTAINERS=("$@")
+
+echo "Container da gestire:"
+for c in "${CONTAINERS[@]}"; do
+  echo " - $c"
+done
+
+
+# Cicla su ogni container passato e gli crea appunto un servizio
+for c in "${CONTAINERS[@]}"; do
+  SERVICE_FILE="/etc/systemd/system/${c}.service"
+
+  if [ -f "$SERVICE_FILE" ]; then
+    echo "Service $c già esistente, skip"
+    continue
+  fi
+
+  sudo tee "$SERVICE_FILE" > /dev/null <<EOF
+[Unit]
+Description=Docker Container $c
+After=docker.service
+Requires=docker.service
+
+[Service]
+Restart=unless-stopped
+ExecStart=/usr/bin/docker start -a $c
+ExecStop=/usr/bin/docker stop $c
+TimeoutStartSec=0
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+  echo "Creato servizio systemd per $c"
+done
+
+
+
+
+systemctl daemon-reload
+for c in "${CONTAINERS[@]}"; do
+  echo "Abilito e avvio servizio $c"
+  sudo systemctl enable "$c"
+  sudo systemctl start "$c"
+done
