667f919de7d8503d9d8ad622ff72883e8d8049d1
Leo95995 <malvolti68@gmail.com>
2025-12-22 21:19:28 +0100

    fix: docker compose deploy

--- 01-vps-administration/03-automation-resilience/docker_compose_deploy.sh
+++ 01-vps-administration/03-automation-resilience/docker_compose_deploy.sh
@@ -4,7 +4,7 @@ set -e
 
 DOCKER_COMPOSE_PATH=$1
 
-if [[  -z $DOCKER_COMPOSE_PATH || ! -f $DOCKER_COMPOSE_PATH ]];then
+if [[  -z "$DOCKER_COMPOSE_PATH" || ! -f "$DOCKER_COMPOSE_PATH" ]];then
 echo "Docker path not specified or not valid "
 exit 1
 
@@ -13,14 +13,15 @@ fi
 # check docker compose 
 # command -v check if the command exists
 #  &> /dev/null -> delete all the output generate from command -v
-if !command -v docker compose  &> /dev/null;then
+if ! command -v docker compose  &> /dev/null;then
     echo "Docker compose not installed "
     exit 1
 fi
-
-
-cd $DOCKER_COMPOSE_PATH
-
+# get the docker compose full path folder
+WORKING_DIR=$(dirname "$DOCKER_COMPOSE_PATH")
+# enter in the working directory
+cd "$WORKING_DIR"
+# remove docker containers
 docker compose down
-
+# build and start everything
 docker compose up -d --build
